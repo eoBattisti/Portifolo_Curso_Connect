a8618ae49d709d8b9db183b10387d9f5693f9540
eoBattisti <nickgabrielbd@gmail.com>
2021-11-05 21:02:56 -0300

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,17 @@
+$( ".change" ).on("click", function() {
+    if( $( "body" ).hasClass( "dark" )) {
+        $( "body" ).removeClass( "dark" );
+        $( ".icone" ).removeClass("fas fa-moon fa-2x");
+        $( ".icone" ).addClass("fas fa-sun fa-2x");
+    } else {
+        $( "body" ).addClass( "dark" );
+        $( ".icone" ).removeClass("fas fa-sun fa-2x");
+        $( ".icone" ).addClass("fas fa-moon fa-2x");
+    }
+});
+
 function openTab(evt, tabName){
-    var i, tabContent, navButton, subMenuButton;
+    var i, tabContent, navButton;
 
     tabContent = document.getElementsByClassName("tabcontent");
     for(i = 0; i <tabContent.length; i++){
@@ -11,12 +23,12 @@ function openTab(evt, tabName){
         navButton[i].className = navButton[i].className.replace(" active", "");
     }
     if(tabName === "AboutMT"){
-        document.getElementById("btnMT").style.borderLeft = "7px solid green";
+        document.getElementById("btnMT").style.borderLeft = "7px solid var(--extra-color)";
 
     }
     if(tabName !== "AboutMT"){
         document.getElementById("AboutMT").style.display = "none";
-        document.getElementById("btnMT").style.borderLeft = "7px solid var(--gray)";
+        document.getElementById("btnMT").style.borderLeft = "7px solid var(--primary-color)";
     }
     
     document.getElementById(tabName).style.display = "flex";
@@ -36,7 +48,7 @@ function openMenuTab(evt, tabName){
     for(i = 0; i < navButton.length; i++){
         navButton[i].className = navButton[i].className.replace(" active", "");
     }
-    document.getElementById("btnMT").style.borderLeft = "7px solid green";
+    document.getElementById("btnMT").style.borderLeft = "7px solid var(--extra-color)";
     document.getElementById(tabName).style.display = "flex";
     evt.currentTarget.className += " active";
 }
